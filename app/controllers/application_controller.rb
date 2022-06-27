003e4adcb8882acf462d71be77830b5754132728
Njoki <kinuthiash@gmail.com>
2022-06-27 16:28:07 +0300

    set up buyer routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,67 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  # SELLER ROUTES
+  #get sellers information 
+  get "/sellers" do   
+    sellers = Seller.all
+    sellers.to_json(include: :items)
+  end
 
+
+  get "/sellers/:id" do
+    sellers = Seller.all.find(params[:id])
+    sellers.to_json(include: :items)
+  end
+
+  # create a seller
+  post "/sellers" do
+    seller = Seller.create(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      contact: params[:contact]
+    )
+    seller.to_json
+  end
+
+  # delete a seller
+  delete "/sellers/:id" do
+    seller = Seller.all.find(params[:id])
+    seller.destroy
+    seller.to_json
+  end
+
+  # update a seller
+  patch "/sellers/:id" do
+    seller = Seller.all.find(params[:id])
+    seller.update(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password],
+      contact: params[:contact]
+    )
+    seller.to_json
+  end
+
+   # BUYER ROUTES
+  get "/items" do   
+    items = Item.all
+    items.to_json(include: :seller)
+  end
+  # # TODO - filter
+  # get "/items/:category" do
+  #   # items = Item.select {|key,value| category: params[:category]}
+  #   itemkeys = Item.select do |k,v|
+  #     k == {params[:category]} 
+  #   end
+
+  #   arr = itemskeys.values
+  #   arr.to_json
+  # end
+
+  get "/items/:id" do
+    items = Item.all.find(params[:id])
+    items.to_json(include: :seller)
+  end
 end
